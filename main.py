0c9686169a5f4f1125b7f0033625f7628d75fea8
n2et4o <117654110+n2et4o@users.noreply.github.com>
2024-01-21 00:14:31 +0100

    fenetre et boucle du jeu

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-import pygame
+import pygame as pg
+pg.init()
 
-pygame.init()
+# fenetre du jeu
+pg.display.set_caption("storm Grief")
+screen = pg.display.set_mode((1080,720))
+background = pg.image.load("Image_du_jeu/Background.jpg")
+
+
+#boucle du jeu
+running = True
+while running == True :
+    for events in pg.event.get(): # event est une liste, events est un élèment de cette liste
+
+        #Affichage de l'arrière plan
+        screen.blit(background, (250, 200))
+        pg.display.flip()
+
+        #fermeture de la fenetre
+        if events.type == pg.QUIT :
+            running = False
+            pg.quit()
